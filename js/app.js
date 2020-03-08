2c893c8559edc0c665671bc0c2101ff918b4fc3f
Andrea Quiceno <luz.quiceno@correounivalle.edu.co>
2020-03-07 23:13:01 -0500

    Adding functionalty to retrieve item data according to search input

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
-(function ()  {
+(function () {
     'use strict';
 
     angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
+    .constant('BaseUrl', "https://davids-restaurant.herokuapp.com");
 
     NarrowItDownController.$inject = ['MenuSearchService'];
-    function getMatchedMenuItems() {
-        var promise = getMenus();
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
 
-        promise.then(function (result) {
-
-        },
-        function (error) {
-
-        }
-    )};
-
-    function getMenus() {
-        var deferred = $q.defer();
-
-        return deferred.promise;
+        menu.logMenuItems = function (searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+            promise.then(function (response) {
+                menu.menuItems = response;
+            })
+            .catch(function (error) {
+                console.log("Something went terribly wrong.");
+            })
+        };
     }
-});
+
+    MenuSearchService.$inject = ['$http', 'BaseUrl']
+    function MenuSearchService($http, BaseUrl) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: "GET",
+                url: (BaseUrl + "/menu_items.json"),
+                params: {
+                    description: searchTerm
+                }
+            }).then(function (result){
+                var foundItems = result.data.menu_items.filter(item => {
+                                            return item.description
+                                                .toString()
+                                                .toLowerCase()
+                                                .indexOf(searchTerm.toLowerCase()) >= 0
+                                            })
+                return foundItems;
+            });
+        }
+    }
+})();
